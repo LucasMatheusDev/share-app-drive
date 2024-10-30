d64d5a1d0bb1881b80a6023f7d59d5e7e8b23763
Lucas Matheus <200dev.lborges@bv.com.br>
2024-10-30 09:06:35 -0300

    upadate

--- bin/share_app_drive.dart
+++ bin/share_app_drive.dart
@@ -1,10 +1,32 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 
-const defaultWebhookUrl = 'YOUR_TEAMS_WEBHOOK_URL';
+const defaultWebhookUrl =
+    'https://prod-108.westus.logic.azure.com:443/workflows/3bc094ec689345609094ff63ad583c46/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=da79bLYuDCu6tAvMQWMSqt5bmzbxrxNy3BI5cJ71lQw';
+const defaulttoken = "";
+
+Timer animateLoading() {
+  final frames = ['-', '\\', '|', '/'];
+  var i = 0;
+  var maxLength = 10;
+
+  final timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+    stdout.write('\r${frames[i]}');
+    i = (i + 1) % frames.length;
+    maxLength--;
+    if (maxLength == 0) {
+      stdout.write('\n');
+      maxLength = 10;
+    }
+  });
+
+  return timer;
+}
 
 void main(List<String> arguments) async {
   final parser = ArgParser()
@@ -18,12 +40,12 @@ void main(List<String> arguments) async {
       'verbose',
       abbr: 'v',
       negatable: false,
+      defaultsTo: true,
       help: 'Show additional command output.',
     )
     ..addFlag(
       'version',
       negatable: false,
-      defaultsTo: true,
       help: 'Print the tool version.',
     )
     ..addOption(
@@ -53,8 +75,9 @@ void main(List<String> arguments) async {
     )
     ..addOption(
       'onedrive-token',
-      mandatory: true,
-      abbr: 'ot',
+      mandatory: false,
+      defaultsTo: defaulttoken,
+      abbr: 'o',
       help: 'The OneDrive access token to upload the app.\n'
           'Find token in https://developer.microsoft.com/en-us/graph/graph-explorer',
     );
@@ -72,7 +95,7 @@ void main(List<String> arguments) async {
   }
 
   final verbose = args['verbose'] as bool;
-  final task = args['task'] as String?;
+  final task = (args['task'] as String?)?.replaceAll('=', '');
   final appType = args['app'] as String?;
   final appName = args['name'] as String?;
   final webhookUrl = args['webhook'] ?? defaultWebhookUrl;
@@ -86,77 +109,108 @@ void main(List<String> arguments) async {
     print(parser.usage);
     exit(1);
   }
-
-  final outputDir = Directory(path.join('build', task.toUpperCase()));
-  if (!await outputDir.exists()) {
-    await outputDir.create(recursive: true);
-  }
+  List<String> urlsApps = [];
 
   if (appType == 'apk' || appType == 'both') {
-    await buildApp('apk', appName, outputDir, verbose);
+    final animationtimer = animateLoading();
+    final outPutFilePath = await buildApp('apk', appName, verbose);
+    urlsApps.add(await uploadToOneDrive(
+      outPutFilePath,
+      task,
+      verbose,
+      onedriveToken,
+    ));
+    animationtimer.cancel();
   }
 
   if (appType == 'ios' || appType == 'both') {
-    await buildApp('ipa', appName, outputDir, verbose);
+    final animationtimer = animateLoading();
+
+    final outPutFilePath = await buildApp('ipa', appName, verbose);
+    urlsApps.add(await uploadToOneDrive(
+      outPutFilePath,
+      task,
+      verbose,
+      onedriveToken,
+    ));
+    animationtimer.cancel();
   }
 
-  await uploadToOneDrive(outputDir, task, verbose, onedriveToken);
+  final animationtimer = animateLoading();
+
   await sendMessageToTeams(
-    'Build $appName para $appType concluído e disponível no OneDrive.',
+    'Build $appName para $appType concluído e disponível no OneDrive em ${urlsApps.join(', ')}',
     verbose,
     webhookUrl,
   );
+  animationtimer.cancel();
 }
 
-Future<void> buildApp(
-    String appType, String appName, Directory outputDir, bool verbose) async {
+Future<String> buildApp(String appType, String appName, bool verbose) async {
   final buildType = appType == 'apk' ? 'apk' : 'ipa';
-  final buildCommand =
-      'flutter build $buildType --release --build-name=$appName --output-dir=${outputDir.path}';
+  final buildCommand = 'build $buildType --release --build-name=$appName';
 
-  if (verbose) print('Executing build command: $buildCommand');
+  if (verbose) {
+    print('Executing build command: $buildCommand');
+    print('Building $appType...');
+    print('Current directory: ${Directory.current}');
+  }
+  // printar o que o comando flutter build esta retornando
 
-  final process = await Process.run('bash', ['-c', buildCommand]);
+  final process = await Process.run(
+    'flutter',
+    buildCommand.split(' '),
+  );
   if (process.exitCode != 0) {
     print('Error building $appType: ${process.stderr}');
     exit(1);
   }
 
-  final buildPath = outputDir.path;
-
+  final buildPath = 'build/app/outputs/flutter-apk/app-release.apk';
   if (verbose) print('Build $appType concluído e salvo em $buildPath.');
+  return buildPath;
 }
 
-Future<void> uploadToOneDrive(
-  Directory outputDir,
-  String folderName,
+Future<String> uploadToOneDrive(
+  String filePath,
+  String drivePath,
   bool verbose,
   String token,
 ) async {
-  final onedriveApiUrl = 'https://graph.microsoft.com/v1.0/drive/root:/';
+  final onedriveApiUrl = 'https://graph.microsoft.com/v1.0/me/drive/root:';
 
-  for (var file in outputDir.listSync()) {
-    final fileName = path.basename(file.path);
-    final uploadUrl = '$onedriveApiUrl$folderName/$fileName:/content';
+  final file = File(filePath);
 
-    if (verbose) print('Uploading $fileName to OneDrive at $uploadUrl');
+  if (!file.existsSync()) {
+    print('Arquivo não encontrado: $filePath');
+    return throw Exception('Arquivo não encontrado: $filePath');
+  }
 
-    final response = await http.put(
-      Uri.parse(uploadUrl),
-      headers: {
-        'Authorization': 'Bearer $token',
-        'Content-Type': 'application/octet-stream',
-      },
-      body: await File(file.path).readAsBytes(),
-    );
+  final fileName = path.basename(filePath);
+  final uploadUrl = '$onedriveApiUrl/$drivePath/$fileName:/content';
 
-    if (response.statusCode == 201) {
-      if (verbose) {
-        print('Upload do arquivo $fileName para o OneDrive concluído.');
-      }
-    } else {
+  if (verbose) print('Uploading $fileName to OneDrive at $uploadUrl');
+
+  final response = await http.put(
+    Uri.parse(uploadUrl),
+    headers: {
+      'Authorization': 'Bearer $defaulttoken',
+      'Content-Type': 'text/plain',
+    },
+    body: file.readAsBytesSync(),
+  );
+
+  if (response.statusCode == 201) {
+    if (verbose) {
+      print('Upload do arquivo $fileName para o OneDrive concluído.');
+    }
+    final jsonResponse = jsonDecode(response.body);
+    return jsonResponse['@microsoft.graph.downloadUrl'] as String;
+  } else {
+    if (verbose) {
       print('Erro no upload para o OneDrive: ${response.body}');
     }
+    return throw Exception('Erro no upload para o OneDrive: ${response.body}');
   }
 }
 
@@ -166,13 +220,22 @@ Future<void> sendMessageToTeams(
 
   final response = await http.post(
     Uri.parse(webhookUrl),
-    headers: {'Content-Type': 'application/json'},
-    body: '{"content": "$message"}',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      'content': message,
+    }),
   );
 
   if (response.statusCode == 200) {
-    if (verbose) print('Mensagem enviada ao Teams.');
+    if (verbose) {
+      print('Mensagem enviada com sucesso para o Teams.');
+    }
   } else {
-    print('Erro ao enviar mensagem ao Teams: ${response.body}');
+    if (verbose) {
+      print('Erro ao enviar mensagem para o Teams: ${response.body}');
+    }
+    throw Exception('Erro ao enviar mensagem para o Teams: ${response.body}');
   }
 }
